9831c4332389c1432e134301ce408a00c1ee3e6b
Tengzicong <t18519249207@163.com>
2026-05-01 14:00:22 +0800

    Update UI: add feature cards, toolbar with B站/抖音, external links open in browser

---
+++ ERROR-CORE小游戏/ERROR-CORE小游戏/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ERROR-CORE小游戏
+//
+//  Created by zicong on 2026/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
